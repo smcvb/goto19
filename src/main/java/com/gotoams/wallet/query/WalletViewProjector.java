3e33625c0dcec844dadf087e219403bfe7ecf82c
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-06-17 10:44:29 +0200

    Clear out command and query logic
    For demo purposes, clear out the Wallet aggregate and the
    WalletViewProjector, so that both can be live coded during the
    presentation. As the focus lies on showing how to segregate this logic
    in two simple components utilizing a message driven API, it should be
    the emphasize during the presentation
    
    #demo

--- src/main/java/com/gotoams/wallet/query/WalletViewProjector.java
+++ src/main/java/com/gotoams/wallet/query/WalletViewProjector.java
@@ -1,16 +1,6 @@
 package com.gotoams.wallet.query;
 
-import com.gotoams.wallet.coreapi.CashDepositedEvent;
-import com.gotoams.wallet.coreapi.CashWithdrawnEvent;
-import com.gotoams.wallet.coreapi.FindAllWalletsQuery;
-import com.gotoams.wallet.coreapi.FindWalletQuery;
-import com.gotoams.wallet.coreapi.WalletCreatedEvent;
-import com.gotoams.wallet.coreapi.WalletView;
-import org.axonframework.eventhandling.EventHandler;
-import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class WalletViewProjector {
@@ -19,32 +9,5 @@ public class WalletViewProjector {
 
     public WalletViewProjector(WalletViewRepository repository) {
         this.repository = repository;
-    }
-
-    @EventHandler
-    public void on(WalletCreatedEvent event) {
-        repository.save(new WalletView(event.getWalletId(), event.getBalance()));
-    }
-
-    @EventHandler
-    public void on(CashWithdrawnEvent event) {
-        repository.findById(event.getWalletId())
-                  .ifPresent(walletView -> walletView.withdraw(event.getAmount()));
-    }
-
-    @EventHandler
-    public void on(CashDepositedEvent event) {
-        repository.findById(event.getWalletId())
-                  .ifPresent(walletView -> walletView.deposit(event.getAmount()));
-    }
-
-    @QueryHandler
-    public WalletView handle(FindWalletQuery query) {
-        return repository.findById(query.getWalletId()).orElse(null);
-    }
-
-    @QueryHandler
-    public List<WalletView> handle(FindAllWalletsQuery query) {
-        return repository.findAll();
     }
 }
